9947e1ebb00b6facd31a99ff31985a767f61b4a4
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-01 14:33:31 +0300

    refactor: вынести MONGODB из переменной окружения

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,5 @@
 const { SECRET_SIGNING_KEY = 'U2FsdGVkX19nV2KreWqHk1BGD+ojOGgl39N93rtj1DVVSeYcdNPGnAQt4PtU2FvJrEiPtoQRACJ0B/yIORhjvQ==' } = process.env;
-const { MONGODB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const MONGODB_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
