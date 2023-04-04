b55674840628361f8c4961d05bff16d15640f91c
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-04 17:51:30 +0300

    refactor: вынести роуты в отдельный файл из app.js

--- backend/app.js
+++ backend/app.js
@@ -10,15 +10,8 @@ const { errors } = require('celebrate');
 const limiter = require('./middlewares/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const routeSignup = require('./routes/signup');
-const routeSignin = require('./routes/signin');
+const router = require('./routes/index');
 
-const auth = require('./middlewares/auth');
-
-const routeUsers = require('./routes/users');
-const routeCards = require('./routes/cards');
-
-const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
 
 const { MONGODB_URL } = require('./utils/constants');
@@ -47,15 +40,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-app.use('/', routeSignup);
-app.use('/', routeSignin);
-
-app.use(auth);
-
-app.use('/users', routeUsers);
-app.use('/cards', routeCards);
-
-app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
+app.use(router);
 
 app.use(errorLogger);
 app.use(errors());
